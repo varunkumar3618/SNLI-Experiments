66d6d41bb612c4f29c28ec232048705dfb2198a6
Ubuntu <ubuntu@ip-172-31-35-2.us-west-2.compute.internal>
2017-02-27 19:18:48 +0000

    fixed a bug in the download script

--- get_datasets.sh
+++ get_datasets.sh
@@ -8,9 +8,9 @@ cd $DATASETS_DIR
 
 # Get the SNLI dataset
 if hash wget 2>/dev/null; then
-    wget http://nlp.stanford.edu/projects/snli/snli_1.0.zip
+  wget http://nlp.stanford.edu/projects/snli/snli_1.0.zip
 else:
-    curl -O http://nlp.stanford.edu/projects/snli/snli_1.0.zip
+  curl -O http://nlp.stanford.edu/projects/snli/snli_1.0.zip
 fi
 unzip snli_1.0.zip
 rm snli_1.0.zip
@@ -21,5 +21,6 @@ if hash wget 2>/dev/null; then
 else
   curl -O http://nlp.stanford.edu/data/glove.6B.zip
 fi
-unzip glove.6B.zip
+mkdir -p glove.6B
+unzip glove.6B.zip -d glove.6B/
 rm glove.6B.zip
