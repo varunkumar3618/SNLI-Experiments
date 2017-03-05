345462bd94854d3606067ff5b9370c321122b6c6
kennyleung-su <kenleung@stanford.edu>
2017-03-04 21:19:15 -0800

    Added useful comments and resolved arithmetic discrepancies between our implementation and Rocktaschel 2015 (see section 2.3 - Attention). There is a bug involving the use of tensordot with tf placeholders in the attention model.

--- src/models/attention.py
+++ src/models/attention.py
@@ -15,22 +15,35 @@ class AttentionModel(SNLIModel):
 
     def add_prediction_op(self):
         with tf.variable_scope("prediction"):
+
+            # Premise and hypothesis embedding tensors, both with shape
+            # [batch_size, max_len_seq, word_embed_dim]
             prem_embed = get_embedding(self.sentence1_placeholder, self._embedding_matrix,
                                        self._update_embeddings)
             hyp_embed = get_embedding(self.sentence2_placeholder, self._embedding_matrix,
                                       self._update_embeddings, reuse=True)
 
+            # Use LSTMs to run through the entire premise and hypothesis vectors.
+            # Initialize the initial hidden state of the hypothesis LSTM using the final premise
+            # hidden state.
             cell = tf.contrib.rnn.LSTMCell(self._hidden_size, use_peepholes=self._use_peepholes)
             with tf.variable_scope("prem_encoder"):
-                prem_states, prem_final_state\
+                # prem_states.shape => [batch_size, max_len_seq, _hidden_size]
+                prem_states, prem_final_state \
                   = tf.nn.dynamic_rnn(cell, prem_embed, dtype=tf.float32,
                                       sequence_length=self.sentence1_lens_placeholder)
+
+            # TODO(kennyleung): determine whether a fresh instance of the LSTMCell is needed.
+            # Also, consider using a delimiter and running through one LSTM instance as in the paper.
             with tf.variable_scope("hyp_encoder"):
+                # hyp_states.shape => [batch_size, max_len_seq, _hidden_size]
                 hyp_states, _ = tf.nn.dynamic_rnn(cell, hyp_embed, initial_state=prem_final_state,
                                                   sequence_length=self.sentence2_lens_placeholder)
+
+                # LSTM final state is a Tensor of shape [batch_size, _hidden_size] squeezed and sliced
+                # from hyp_states[:, -1, :].
                 final_hyp_state = tf.squeeze(
-                    tf.slice(hyp_states, [0, tf.shape(hyp_states)[1] - 1, 0], [-1, 1, -1]),
-                    axis=1
+                    tf.slice(hyp_states, begin=[0, tf.shape(hyp_states)[1] - 1, 0], size=[-1, 1, -1])
                 )
 
             with tf.variable_scope("attention"):
@@ -45,10 +58,16 @@ class AttentionModel(SNLIModel):
                 W_x = tf.get_variable("W_x", shape=[self._hidden_size, self._hidden_size],
                                       initializer=tf.contrib.layers.xavier_initializer())
 
-                M = matmul_3d_2d(prem_states, W_y, lastdim=True)\
-                    + tf.expand_dims(tf.matmul(final_hyp_state, W_h), axis=1)
-                alpha = tf.nn.softmax(matmul_3d_1d(M, w))
-                r = tf.reduce_sum(prem_states + tf.expand_dims(alpha, axis=2), axis=2)
+                # Matrix-multiply each horizontal slice of prem_states by weight matrix W_y,
+                # and use broadcasting to add outer(W_h*h_n, e_L).
+                # M.shape => [batch_size, max_len_seq, _hidden_size]
+                M = tf.tanh(tf.tensordot(prem_states, W_y, axes=[[2], [1]]) \
+                    + tf.expand_dims(tf.matmul(final_hyp_state, W_h), axis=1))
+
+                # alpha.shape => [batch_size, max_len_seq]
+                alpha = tf.nn.softmax(tf.tensordot(M, w, axes=1))
+                # r.shape => [batch_size, _hidden_size]
+                r = tf.tensordot(prem_states, alpha, axes=[[2], [0]])
 
                 logits = tf.tanh(tf.matmul(r, W_p) + tf.matmul(final_hyp_state, W_x))
                 preds = tf.argmax(logits, axis=1)
