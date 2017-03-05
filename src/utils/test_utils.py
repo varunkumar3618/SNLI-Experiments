08e2abf0c04d2ee59ce3a0df313d9aeb47394202
kennyleung-su <kenleung@stanford.edu>
2017-03-04 21:13:55 -0800

    Added batch matmul operations (i.e. 3dx2d, 3dx1d) helper functions using tf.tensordot. Verified with unit tests.

---
+++ src/utils/test_utils.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+def test_all_close(name, actual, expected):
+    if actual.shape != expected.shape:
+        raise ValueError("{:} failed, expected output to have shape {:} but has shape {:}"
+                         .format(name, expected.shape, actual.shape))
+    if np.amax(np.fabs(actual - expected)) > 1e-6:
+        raise ValueError("{:} failed, expected {:} but value is {:}".format(name, expected, actual))
+    else:
+        print name, "passed!"
+        
