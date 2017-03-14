db7f501365cff3c7bb5a845d523f4f4989966be6
Varun Vijay <varunkumar3618@gmail.com>
2017-03-14 09:47:00 -0700

    single

--- src/models/stacked.py
+++ src/models/stacked.py
@@ -51,17 +51,23 @@ class StackedAttentionModel(SNLIModel):
                 use_peepholes=self._use_peepholes,
                 initializer=self.rec_init
             )
-            with tf.variable_scope("prem_encoder"):
-                (prem_fw_hiddens, prem_bw_hiddens), prem_final_state\
-                    = tf.nn.bidirectional_dynamic_rnn(cell, cell, prem, dtype=tf.float32,
-                                                      sequence_length=self.sentence1_lens_placeholder)
-                prem_hiddens = tf.concat([prem_fw_hiddens, prem_bw_hiddens], axis=2)
+            # with tf.variable_scope("prem_encoder"):
+            #     (prem_fw_hiddens, prem_bw_hiddens), prem_final_state\
+            #         = tf.nn.bidirectional_dynamic_rnn(cell, cell, prem, dtype=tf.float32,
+            #                                           sequence_length=self.sentence1_lens_placeholder)
+            #     prem_hiddens = tf.concat([prem_fw_hiddens, prem_bw_hiddens], axis=2)
+            prem_hiddens, prem_final_state \
+              = tf.nn.dynamic_rnn(cell, prem_proj, dtype=tf.float32,
+                                  sequence_length=self.sentence1_lens_placeholder)
 
-            with tf.variable_scope("hyp_encoder"):
-                (hyp_fw_hiddens, hyp_bw_hiddens), hyp_final_state \
-                  = tf.nn.bidirectional_dynamic_rnn(cell, cell, prem, dtype=tf.float32,
-                                      sequence_length=self.sentence1_lens_placeholder)
-                hyp_hiddens = tf.concat([hyp_fw_hiddens, hyp_bw_hiddens], axis=2)
+            # with tf.variable_scope("hyp_encoder"):
+            #     (hyp_fw_hiddens, hyp_bw_hiddens), hyp_final_state \
+            #       = tf.nn.bidirectional_dynamic_rnn(cell, cell, prem, dtype=tf.float32,
+            #                           sequence_length=self.sentence1_lens_placeholder)
+            #     hyp_hiddens = tf.concat([hyp_fw_hiddens, hyp_bw_hiddens], axis=2)
+            hyp_hiddens, hyp_final_state\
+                = tf.nn.dynamic_rnn(cell, hyp_proj, initial_state=prem_final_state,
+                                    sequence_length=self.sentence2_lens_placeholder)
         return prem_hiddens, prem_final_state, hyp_hiddens, hyp_final_state
 
     def attention(self, x, y, scope):
